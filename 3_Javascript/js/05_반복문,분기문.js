21e989bf9672369c8fa60ddaea315bb94748fc21
ghddbsrl1234 <qoxmfdbsrl@gmail.com>
2025-11-12 16:41:14 +0900

    2025/11/12

--- 3_Javascript/js/05_반복문,분기문.js
+++ 3_Javascript/js/05_반복문,분기문.js
@@ -40,4 +40,63 @@ while(input !== null) { // 취소 버튼을 눌렀을 때 멈춤
 alert(`김밥 : ${gCount}, 라면 : ${rCount}, 돈까스 : ${dCount}`);
 let sum = (gCount * gimbap) + (rCount * ramen) + (donkkaseu * dCount);
 alert(`총 가격 : ${sum} 원`);
+
+}
+
+//Up & Down 게임
+function stratgame() {
+    // 맞춰야 하는 난수(1~200) 발생시켜 저장
+    const answer = Math.floor(Math.random() * 200 ) + 1;
+
+    // 정답 시도 회수를 세기 위한 변수 선언
+    let count = 0;
+
+  // prompt에 출력할 문자열
+  let str = "1부터 200사이 숫자 입력";
+
+  while(true) {
+    
+    let input = prompt(str);
+
+    if(input === null) {
+      alert("게임 포기")
+      break;
+    }
+
+    // 숫자 입력 후 확인 클릭 시
+    const value = Number(input); // 입력 받은 값 숫자로 변환
+
+    // 숫자가 아닌 문자열이 잘못입력된 경우
+    // NaN (Not a Number : 숫자가 아니다)
+    // isNaN(값) : 값이 NaN이면 true
+    if(isNaN(value) ) { // 숫자가 아닌 값을 입력한 경우
+      alert("숫자만 입력해주세요");
+      continue;
+    }
+
+    if(value < 1 || value > 200) { // 범위 초과
+      alert("1~200 사이 값만 작성해주세요");
+      continue;
+    }
+
+    // 정답을 맞추기 위한 시도를 했기 때문에 count 를 1 증가
+    count++;
+
+    // 정답인 경우
+    if(value === answer) {
+      alert(`정답 !!! (${answer}) / 시도 횟수 : ${count}`);
+      break;
+    }
+
+    // 정답이 아닌 경우
+    if(value < answer) { // 작은 경우
+      str = `${value} [Up] / 시도 횟수 : ${count}`;
+
+    } else { // 큰 경우
+        str = `${value} [Down] / 시도 횟수 : ${count}`;
+        
+    }
+
+  }
+
 }
